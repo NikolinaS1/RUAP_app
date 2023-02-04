e9876d5347b722e5187811dcc3fa5cfbb552a0ea
DESKTOP-VR8NC81\Nikolina <nikolina.stjepanovic@student.ferit.hr>
2023-02-04 16:17:28 +0100

    graphs and design

--- nextjs-blog/server.js
+++ nextjs-blog/server.js
@@ -1,8 +1,7 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-var http = require('http');
-
+var http = require("http");
 
 const express = require("express");
 const app = express();
@@ -71,15 +70,14 @@ app.get("/register", checkNotAuthenticated, (req, res) => {
   res.render("register.ejs");
 });
 
-
-
-
-
-
-
-app.post("/login", checkNotAuthenticated, passport.authenticate("local", {
-  successRedirect: "/",failureRedirect: "/login",failureFlash: true,
-  }) 
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
 );
 
 app.post("/register", checkNotAuthenticated, async (req, res) => {
@@ -98,122 +96,178 @@ app.post("/register", checkNotAuthenticated, async (req, res) => {
   }
 });
 
-app.post("/predictor",(req,res) => {
+app.post("/predictor", (req, res) => {
   const request = req.body;
-  
+
   const dataState = {
-  Gender: request.gender || 'value',
-  Age: request.Age || 'value',
-  Height: request.Height || 'value',
-  Weight: request.Weight || 'value',
-  family_history_with_overweight: request.family_history_with_overweight || 'value',
-  FAVC: request.FAVC || 'value',
-  FCVC: request.FCVC|| '0',
-  NCP: request.NCP || '0',
-  CAEC: request.CAEC || 'value',
-  SMOKE: request.SMOKE || 'value',
-  CH2O: request.CH2O || '0',
-  SCC: request.SCC || 'value',
-  FAF: request.FAF || '0',
-  TUE: request.TUE || '0',
-  CALC: request.CALC|| 'value',
-  MTRANS: request.MTRANS || 'value',
-  NObeyesdad: 'value',
-  }
+    Gender: request.gender || "value",
+    Age: request.Age || "value",
+    Height: request.Height || "value",
+    Weight: request.Weight || "value",
+    family_history_with_overweight:
+      request.family_history_with_overweight || "value",
+    FAVC: request.FAVC || "value",
+    FCVC: request.FCVC || "0",
+    NCP: request.NCP || "0",
+    CAEC: request.CAEC || "value",
+    SMOKE: request.SMOKE || "value",
+    CH2O: request.CH2O || "0",
+    SCC: request.SCC || "value",
+    FAF: request.FAF || "0",
+    TUE: request.TUE || "0",
+    CALC: request.CALC || "value",
+    MTRANS: request.MTRANS || "value",
+    NObeyesdad: "value",
+  };
   console.log(dataState);
 
-  
-
-    //API
+  //API
+  const fetch = require("node-fetch");
+  global.fetch = fetch;
+  global.Headers = fetch.Headers;
 
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Authorization", "Bearer 9eF905yGqTk48Cn+kNcsbDrrqmklt5aVSH7hClQe8Kti64TV6eaZeeCmbi1qTvYn356IoXArHvU1+AMCe4F5+A==");
+  myHeaders.append("Authorization", "Bearer " + process.env.API_KEY);
 
   var NoJSONraw = {
-
-    "Inputs": {
-
-            "input1":
-            {
-                "ColumnNames": ["Gender", "Age", "Height", "Weight", "family_history_with_overweight", "FAVC", "FCVC", "NCP", "CAEC", "SMOKE", "CH2O", "SCC", "FAF", "TUE", "CALC", "MTRANS", "NObeyesdad"],
-                "Values": [ [ dataState.Gender, dataState.Age, dataState.Height, dataState.Weight, dataState.family_history_with_overweight, dataState.FAVC, dataState.FCVC, dataState.NCP, dataState.CAEC, dataState.SMOKE, dataState.CH2O, dataState.SCC, dataState.FAF, dataState.TUE, dataState.CALC, dataState.MTRANS, dataState.NObeyesdad ], ]
-            },        },
-        "GlobalParameters": {
-  }}
+    Inputs: {
+      input1: {
+        ColumnNames: [
+          "Gender",
+          "Age",
+          "Height",
+          "Weight",
+          "family_history_with_overweight",
+          "FAVC",
+          "FCVC",
+          "NCP",
+          "CAEC",
+          "SMOKE",
+          "CH2O",
+          "SCC",
+          "FAF",
+          "TUE",
+          "CALC",
+          "MTRANS",
+          "NObeyesdad",
+        ],
+        Values: [
+          [
+            dataState.Gender,
+            dataState.Age,
+            dataState.Height,
+            dataState.Weight,
+            dataState.family_history_with_overweight,
+            dataState.FAVC,
+            dataState.FCVC,
+            dataState.NCP,
+            dataState.CAEC,
+            dataState.SMOKE,
+            dataState.CH2O,
+            dataState.SCC,
+            dataState.FAF,
+            dataState.TUE,
+            dataState.CALC,
+            dataState.MTRANS,
+            dataState.NObeyesdad,
+          ],
+        ],
+      },
+    },
+    GlobalParameters: {},
+  };
   var raw = JSON.stringify(NoJSONraw);
   console.log(raw);
 
-
-
   var requestOptions = {
-    method: 'POST',
+    method: "POST",
     headers: myHeaders,
     body: raw,
-    redirect: 'follow'
+    redirect: "follow",
   };
 
-
-  fetch("https://ussouthcentral.services.azureml.net/workspaces/8bde0f43448a4c0a9399fc10298e259f/services/ecaf4910997046428eac705b610d6858/execute?api-version=2.0&details=true", requestOptions)
-    .then(response => response.text())
-    .then(result => { 
-      console.log(result); 
+  fetch(
+    "https://ussouthcentral.services.azureml.net/workspaces/8bde0f43448a4c0a9399fc10298e259f/services/ecaf4910997046428eac705b610d6858/execute?api-version=2.0&details=true",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result);
       //data = JSON.stringify(result);
       // res.send(data);
-      var sp = result.split(',');
+      var sp = result.split(",");
       sp[60] = sp[60].split('"');
-      var temp = sp[60][1].split('_');
+      var temp = sp[60][1].split("_");
       console.log(temp);
       var oblvl = "";
       for (let i = 0; i < temp.length; i++) {
         oblvl += temp[i] + " ";
       }
       //console.log(sp[60][1]);
-      res.write('<!DOCTYPE html>'+
-            '<link rel="stylesheet" type="text/css" href="styles/results.css" />'+
-            '<html>'+
-            '    <head>'+
-            '        <meta charset="utf-8" />'+
-            '        <title>Predictor</title>'+
-            '        <meta name="description" content="Generated by create next app" />'+
-            '        <meta name="viewport" content="width=device-width, initial-scale=1" />'+
-            '        <link rel="icon" href="./OP.jpg" />'+
-            '    </head>'+ 
-            '    <body>'+
-            '        <header>'+
-            '           <img class="logo" src="./OP_logo_3.jpg"></img>'+
-            '           <nav>'+
-            '             <ul class="nav_links">'+
-            '               <li><a href="/">Home</a></li>'+
-            '               <li><a href="/predictor">Predictor</a></li>'+
-            '             </ul>'+
-            '           </nav>'+
-            '           <div class="nav_links">'+
-            '             <ul>'+
-            '               <li>'+
-            '                   <form action="/logout?_method=DELETE" method="POST" class="logout">'+
-            '                     <button type="submit">Log Out</button>'+
-            '                   </form>'+
-            '               </li>'+
-            '               <li><p class="profile">Name</p></li>'+ 
-            '             </ul>'+
-            '           </div>'+
-            '         </header>'+
-            '         <div class="res">'+
-            `           <p id="result">Your obesity level is: ${oblvl}</p>`+
-            '         </div>'+
-            '    </body>'+
-            '</html>');
-            res.end();
+      res.write(
+        "<!DOCTYPE html>" +
+          "<html>" +
+          "   <head>" +
+          '       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">' +
+          '       <meta charset="utf-8" />' +
+          "       <title>Obesity predictor</title>" +
+          '       <meta name="viewport" content="width=device-width, initial-scale=1" />' +
+          '       <link rel="icon" href="./OP.png" />' +
+          "   </head>" +
+          "   <body>" +
+          '       <nav class="navbar navbar-expand-sm bg-primary navbar-dark">' +
+          '           <div class="container-fluid">' +
+          '               <img class="logo" src="logo.png" width="190px"></img>' +
+          '               <ul class="navbar-nav">' +
+          '                   <li class="nav-item"><a class="nav-link active" href="/">Home</a></li>' +
+          '                   <li class="nav-item"><a class ="nav-link" href="/predictor">Predictor</a></li>' +
+          '                   <li class="nav-item">' +
+          '                       <form action="/logout?_method=DELETE" method="POST" class="logout">' +
+          '                           <button class="btn btn-primary" type="submit">Log Out</button>' +
+          "                       </form>" +
+          "                   </li>" +
+          "               </ul>" +
+          "           </div>" +
+          "       </nav>" +
+          '       <div class="res">' +
+          `           <h2 id="result" class="text-center mb-4 mt-4 text-secondary">Your obesity level is: ${oblvl}</h2>` +
+          "       </div>" +
+          '      <div class="mt-5 table-responsive">' +
+          '         <table class="table-bordered mx-auto w-auto">' +
+          "         <thead>" +
+          '             <tr class="text-secondary text-center" height="50">' +
+          '                 <th width="140">Underweight</th>' +
+          '                 <th width="140">Normal weight</th>' +
+          '                 <th width="140">Overweight1</th>' +
+          '                 <th width="140">Overweight2</th>' +
+          '                 <th width="140">Obesity1</th>' +
+          '                 <th width="140">Obesity2</th>' +
+          '                 <th width="140">Obesity3</th>' +
+          "             </tr>" +
+          "         </thead>" +
+          "         <tbody>" +
+          '             <tr class="text-secondary text-center" height="50">' +
+          "                 <td>11%</td>" +
+          "                 <td></td>" +
+          "                 <td></td>" +
+          "                 <td></td>" +
+          "                 <td></td>" +
+          "                 <td></td>" +
+          "                 <td></td>" +
+          "             </tr>" +
+          "         </tbody>" +
+          "       </table>" +
+          "     </div>" +
+          "   </body>" +
+          "</html>"
+      );
+      res.end();
     })
-    .catch(error => console.log('error', error));
-    //res.redirect("/predictor");
-   //res.render('predictor', {title: 'POST test'});
-  }); 
-
-
-
-
+    .catch((error) => console.log("error", error));
+  //res.redirect("/predictor");
+  //res.render('predictor', {title: 'POST test'});
+});
 
 app.delete("/logout", function (req, res, next) {
   req.logOut(function (err) {
@@ -238,32 +292,6 @@ function checkNotAuthenticated(req, res, next) {
   }
   next();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, function () {
   console.log("App is running on Port 3000");
